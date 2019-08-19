b54555e5d675b03fc8e16837e66d76a32f1af0cb
DESKTOP-9CSDO96\evpjo <ev.p.jones1@gmail.com>
2019-08-19 11:42:26 -0400

    added additional CAS templates

--- source/Template.lua
+++ source/Template.lua
@@ -2,10 +2,25 @@ redCAS = {
   "429 Motor Rgt",
   "503 Motor Rgt",
   "693 Motor Rgt",
+  "10 Bat",
+  "11 Bat",
+  "13 _15 Bat",
+  "SO Tank Brig",
+  "136 Guards Rifle Brig_205 Motor Rifle Brig",
+  "135 Motorized Rifle Rgt",
+  "1128 Anti_Tank Rgt",
+  "73_76 Motorized Brig",
+  "33_34 Motor Rifle Rgt.",
+  
 }
 
 blueCAS = {
   "41_42_43 Mech Inf Bat",
   "11 Talavi Light Inf Bat",
-  "12_13_21_22_23_31_32_33 Light Inf Batt",
+  "12_13_21_22_23_31_32_33 Light Inf Bat",
+  "3 Bat 1 Marines Company I",
+  "3 Bat 1 Marines Company K",
+  "3 Bat 1 Marines Company L",
+  "13 MEU Tank Platoon",
+  "1 Light Armored Rec Bat",
 }
